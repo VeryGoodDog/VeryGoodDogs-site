96a722beb9a95b3eccdabdba9b22dd7c8f36aea9
VeryGoodDog <alex.h@me.com>
2017-05-16 08:53:15 -0400

    header fixed

--- head.php
+++ head.php
@@ -1,4 +1,4 @@
-<!-- <link rel="stylesheet" href="main.css"> -->
+<link rel="stylesheet" href="main.css">
 <link rel="icon" href="VGD logo.png">
 <script src="/jquery/jquery-3.1.1.js"></script>
 <script src="/jquery/jquery-ui.js"></script>
--- header.php
+++ header.php
@@ -23,8 +23,12 @@ function size(el,height,width) {
   .height(height)
   .width(width);
 }
-  size('#sizer',$('#header').height(),'100%');
+  size('#sizer',$('.header').height(),'100%');
   $(window).resize(function (event) {
-    size('#sizer',$('#header').height(),'100%');
+    size('#sizer',$('.header').height(),'100%');
   });
 </script>
+<div id="links">
+  <a class="link" href="/links/index.php">Links:</a>
+  <a class="link" href="https://github.com/VeryGoodDog">My GitHub</a>
+</div>
