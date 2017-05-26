569fbd75b14e3d87eb80bbf5d2690f3bd6a4a6c4
VeryGoodDog <alex.h@me.com>
2017-05-26 10:02:14 -0400

    new fonts

--- head.php
+++ head.php
@@ -1,3 +1,5 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:100,400|Roboto:300,400" rel="stylesheet">
 <link rel="stylesheet" href="main.css">
 <link rel="icon" href="VGD logo.png">
 <script src="/jquery/jquery-3.1.1.js"></script>
