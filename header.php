7fe5d65f74256c8f13b893c59d5422fe0fe5370b
VeryGoodDog <alex.h@me.com>
2017-05-29 09:56:53 -0400

    header improvements

--- header.php
+++ header.php
@@ -16,18 +16,6 @@
     </h2>
   </div>
 </header>
-<div id="sizer"></div>
-<script>
-function size(el,height,width) {
-  $(el)
-  .height(height)
-  .width(width);
-}
-  size('#sizer',$('.header').height(),'100%');
-  $(window).resize(function (event) {
-    size('#sizer',$('.header').height(),'100%');
-  });
-</script>
 <div id="links">
   <a class="link" href="/links/index.php">Links:</a>
   <a class="link" href="https://github.com/VeryGoodDog">My GitHub</a>
