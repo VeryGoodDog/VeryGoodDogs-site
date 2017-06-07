ec51ca1d9f6eb16d1c5f9a66b84a1aa932dcb57b
VeryGoodDog <alex.h@me.com>
2017-06-07 09:12:02 -0400

    minor font improvements

--- include/head/head.php
+++ include/head/head.php
@@ -1,6 +1,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:100,400|Roboto:300,400">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css">
+<link rel="stylesheet" href="/include/main.css">
 <link rel="stylesheet" href="main.css">
 <link rel="stylesheet" href="/include/showcase.css">
 <link rel="stylesheet" href="/include/jquery-ui.css">
@@ -9,3 +10,4 @@
 <script src="/include/jquery-ui.js"></script>
 <script src="/include/plugins.js"></script>
 <script src="/include/baseHelpers.js"></script>
+<script src="main.js"></script>
