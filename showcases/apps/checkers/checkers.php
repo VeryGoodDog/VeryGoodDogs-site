da6eab3c519d603c90eceb0f8c1a59c3228c34c3
VeryGoodDog <alex.h@me.com>
2017-07-27 12:43:14 -0400

    alteration to structure of checkers

--- showcases/apps/checkers/checkers.php
+++ showcases/apps/checkers/checkers.php
@@ -3,161 +3,167 @@
 </head>
 <body>
 <link rel="stylesheet" href="main.css">
-<div id="checkersHeader">
-  <div id="playerCont">
-    <div class="playerMessage" id="playerTag">
-      The player is:
+<div id="gameWrapper">
+  <div id="checkersHeader">
+    <div id="playerCont">
+      <div class="playerMessage" id="playerTag">
+        The player is:
+      </div>
+      <div class="playerMessage" id="playerBoard">
+      </div>
     </div>
-    <div class="playerMessage" id="playerBoard">
+    <div id="restartButton">Restart</div>
+  </div>
+  <div id="boardWrapper">
+    <div id="board">
+    <div class="c 1">
+      <div class="r red" data-row="1" data-column="1">
+      </div>
+      <div class="r white" data-row="2" data-column="1">
+      </div>
+      <div class="r red" data-row="3" data-column="1">
+      </div>
+      <div class="r white" data-row="4" data-column="1">
+      </div>
+      <div class="r red" data-row="5" data-column="1">
+      </div>
+      <div class="r white" data-row="6" data-column="1">
+      </div>
+      <div class="r red" data-row="7" data-column="1">
+      </div>
+      <div class="r white" data-row="8" data-column="1">
+      </div>
+    </div>
+    <div class="c 2">
+      <div class="r white" data-row="1" data-column="2">
+      </div>
+      <div class="r red" data-row="2" data-column="2">
+      </div>
+      <div class="r white" data-row="3" data-column="2">
+      </div>
+      <div class="r red" data-row="4" data-column="2">
+      </div>
+      <div class="r white" data-row="5" data-column="2">
+      </div>
+      <div class="r red" data-row="6" data-column="2">
+      </div>
+      <div class="r white" data-row="7" data-column="2">
+      </div>
+      <div class="r red" data-row="8" data-column="2">
+      </div>
+    </div>
+    <div class="c 3">
+      <div class="r red" data-row="1" data-column="3">
+      </div>
+      <div class="r white" data-row="2" data-column="3">
+      </div>
+      <div class="r red" data-row="3" data-column="3">
+      </div>
+      <div class="r white" data-row="4" data-column="3">
+      </div>
+      <div class="r red" data-row="5" data-column="3">
+      </div>
+      <div class="r white" data-row="6" data-column="3">
+      </div>
+      <div class="r red" data-row="7" data-column="3">
+      </div>
+      <div class="r white" data-row="8" data-column="3">
+      </div>
+    </div>
+    <div class="c 4">
+      <div class="r white" data-row="1" data-column="4">
+      </div>
+      <div class="r red" data-row="2" data-column="4">
+      </div>
+      <div class="r white" data-row="3" data-column="4">
+      </div>
+      <div class="r red" data-row="4" data-column="4">
+      </div>
+      <div class="r white" data-row="5" data-column="4">
+      </div>
+      <div class="r red" data-row="6" data-column="4">
+      </div>
+      <div class="r white" data-row="7" data-column="4">
+      </div>
+      <div class="r red" data-row="8" data-column="4">
+      </div>
+    </div>
+    <div class="c 5">
+      <div class="r red" data-row="1" data-column="5">
+      </div>
+      <div class="r white" data-row="2" data-column="5">
+      </div>
+      <div class="r red" data-row="3" data-column="5">
+      </div>
+      <div class="r white" data-row="4" data-column="5">
+      </div>
+      <div class="r red" data-row="5" data-column="5">
+      </div>
+      <div class="r white" data-row="6" data-column="5">
+      </div>
+      <div class="r red" data-row="7" data-column="5">
+      </div>
+      <div class="r white" data-row="8" data-column="5">
+      </div>
+    </div>
+    <div class="c 6">
+      <div class="r white" data-row="1" data-column="6">
+      </div>
+      <div class="r red" data-row="2" data-column="6">
+      </div>
+      <div class="r white" data-row="3" data-column="6">
+      </div>
+      <div class="r red" data-row="4" data-column="6">
+      </div>
+      <div class="r white" data-row="5" data-column="6">
+      </div>
+      <div class="r red" data-row="6" data-column="6">
+      </div>
+      <div class="r white" data-row="7" data-column="6">
+      </div>
+      <div class="r red" data-row="8" data-column="6">
+      </div>
+    </div>
+    <div class="c 7">
+      <div class="r red" data-row="1" data-column="7">
+      </div>
+      <div class="r white" data-row="2" data-column="7">
+      </div>
+      <div class="r red" data-row="3" data-column="7">
+      </div>
+      <div class="r white" data-row="4" data-column="7">
+      </div>
+      <div class="r red" data-row="5" data-column="7">
+      </div>
+      <div class="r white" data-row="6" data-column="7">
+      </div>
+      <div class="r red" data-row="7" data-column="7">
+      </div>
+      <div class="r white" data-row="8" data-column="7">
+      </div>
+    </div>
+    <div class="c 8">
+      <div class="r white" data-row="1" data-column="8">
+      </div>
+      <div class="r red" data-row="2" data-column="8">
+      </div>
+      <div class="r white" data-row="3" data-column="8">
+      </div>
+      <div class="r red" data-row="4" data-column="8">
+      </div>
+      <div class="r white" data-row="5" data-column="8">
+      </div>
+      <div class="r red" data-row="6" data-column="8">
+      </div>
+      <div class="r white" data-row="7" data-column="8">
+      </div>
+      <div class="r red" data-row="8" data-column="8">
+      </div>
     </div>
   </div>
-  <div id="restartButton">Restart</div>
+  </div>
+  <div id="bottomSpacer">
+  </div>
 </div>
-<div id="board">
-  <div class="c 1">
-    <div class="r red" data-row="1" data-column="1">
-    </div>
-    <div class="r white" data-row="2" data-column="1">
-    </div>
-    <div class="r red" data-row="3" data-column="1">
-    </div>
-    <div class="r white" data-row="4" data-column="1">
-    </div>
-    <div class="r red" data-row="5" data-column="1">
-    </div>
-    <div class="r white" data-row="6" data-column="1">
-    </div>
-    <div class="r red" data-row="7" data-column="1">
-    </div>
-    <div class="r white" data-row="8" data-column="1">
-    </div>
-  </div>
-  <div class="c 2">
-    <div class="r white" data-row="1" data-column="2">
-    </div>
-    <div class="r red" data-row="2" data-column="2">
-    </div>
-    <div class="r white" data-row="3" data-column="2">
-    </div>
-    <div class="r red" data-row="4" data-column="2">
-    </div>
-    <div class="r white" data-row="5" data-column="2">
-    </div>
-    <div class="r red" data-row="6" data-column="2">
-    </div>
-    <div class="r white" data-row="7" data-column="2">
-    </div>
-    <div class="r red" data-row="8" data-column="2">
-    </div>
-  </div>
-  <div class="c 3">
-    <div class="r red" data-row="1" data-column="3">
-    </div>
-    <div class="r white" data-row="2" data-column="3">
-    </div>
-    <div class="r red" data-row="3" data-column="3">
-    </div>
-    <div class="r white" data-row="4" data-column="3">
-    </div>
-    <div class="r red" data-row="5" data-column="3">
-    </div>
-    <div class="r white" data-row="6" data-column="3">
-    </div>
-    <div class="r red" data-row="7" data-column="3">
-    </div>
-    <div class="r white" data-row="8" data-column="3">
-    </div>
-  </div>
-  <div class="c 4">
-    <div class="r white" data-row="1" data-column="4">
-    </div>
-    <div class="r red" data-row="2" data-column="4">
-    </div>
-    <div class="r white" data-row="3" data-column="4">
-    </div>
-    <div class="r red" data-row="4" data-column="4">
-    </div>
-    <div class="r white" data-row="5" data-column="4">
-    </div>
-    <div class="r red" data-row="6" data-column="4">
-    </div>
-    <div class="r white" data-row="7" data-column="4">
-    </div>
-    <div class="r red" data-row="8" data-column="4">
-    </div>
-  </div>
-  <div class="c 5">
-    <div class="r red" data-row="1" data-column="5">
-    </div>
-    <div class="r white" data-row="2" data-column="5">
-    </div>
-    <div class="r red" data-row="3" data-column="5">
-    </div>
-    <div class="r white" data-row="4" data-column="5">
-    </div>
-    <div class="r red" data-row="5" data-column="5">
-    </div>
-    <div class="r white" data-row="6" data-column="5">
-    </div>
-    <div class="r red" data-row="7" data-column="5">
-    </div>
-    <div class="r white" data-row="8" data-column="5">
-    </div>
-  </div>
-  <div class="c 6">
-    <div class="r white" data-row="1" data-column="6">
-    </div>
-    <div class="r red" data-row="2" data-column="6">
-    </div>
-    <div class="r white" data-row="3" data-column="6">
-    </div>
-    <div class="r red" data-row="4" data-column="6">
-    </div>
-    <div class="r white" data-row="5" data-column="6">
-    </div>
-    <div class="r red" data-row="6" data-column="6">
-    </div>
-    <div class="r white" data-row="7" data-column="6">
-    </div>
-    <div class="r red" data-row="8" data-column="6">
-    </div>
-  </div>
-  <div class="c 7">
-    <div class="r red" data-row="1" data-column="7">
-    </div>
-    <div class="r white" data-row="2" data-column="7">
-    </div>
-    <div class="r red" data-row="3" data-column="7">
-    </div>
-    <div class="r white" data-row="4" data-column="7">
-    </div>
-    <div class="r red" data-row="5" data-column="7">
-    </div>
-    <div class="r white" data-row="6" data-column="7">
-    </div>
-    <div class="r red" data-row="7" data-column="7">
-    </div>
-    <div class="r white" data-row="8" data-column="7">
-    </div>
-  </div>
-  <div class="c 8">
-    <div class="r white" data-row="1" data-column="8">
-    </div>
-    <div class="r red" data-row="2" data-column="8">
-    </div>
-    <div class="r white" data-row="3" data-column="8">
-    </div>
-    <div class="r red" data-row="4" data-column="8">
-    </div>
-    <div class="r white" data-row="5" data-column="8">
-    </div>
-    <div class="r red" data-row="6" data-column="8">
-    </div>
-    <div class="r white" data-row="7" data-column="8">
-    </div>
-    <div class="r red" data-row="8" data-column="8">
-    </div>
-  </div>
-</div>
-<script src="main.js"></script>
+<script src="checkers.js"></script>
 </body>
